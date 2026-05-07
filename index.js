3d7284806c802fceaf524ba3d452c494df97c9c5
sathiyavizza <sathiyam8754@gmail.com>
2026-05-07 16:46:00 +0530

    limit increage

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ import productRoutes from './routes/product.routes.js';
 const app = express();
 dotenv.config();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
  connectDB();
